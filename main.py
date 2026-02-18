3ef4e9c865d45c6e412cdace8fca529cfb2d8e9a
Lily Farris <lilycatherinefarris@hotmail.com>
2026-02-18 12:48:43 -0500

    Changed variable names

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from SavingsAccount import SavingsAccount
 
 def main():
 
-    account1 = CheckingAccount("Lily Farris", 300, 200, 120, 499, 123)
+    checking_account1 = CheckingAccount("Lily Farris", 300, 200, 120, 499, 123)
 
     #Test transfer above transfer limit
-    account1.transfer_money(500)
+    checking_account1.transfer_money(500)
     #Works Correctly
 
     #Test transfer below minimum balance
-    account1.transfer_money(110)
+    checking_account1.transfer_money(110)
     #Works correctly
 
     #Test successful transfer
-    account1.transfer_money(60)
+    checking_account1.transfer_money(60)
     #Works correctly
+
+
 
 main()
