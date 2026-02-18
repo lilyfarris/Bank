67269d73bd15ae77bf89ac6eb330799af6d2d6e3
Lily Farris <lilycatherinefarris@hotmail.com>
2026-02-18 12:31:15 -0500

    Added transfer_money method that makes sure transfer is not greater than transfer limit or less that minimum balance

--- CheckingAccount.py
+++ CheckingAccount.py
@@ -4,3 +4,11 @@ class CheckingAccount(BankAccount):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self.transfer_limit = transfer_limit
 
+    def transfer_money(self, amount_to_transfer):
+        if amount_to_transfer >= self.transfer_limit:
+            print("This is above your transfer limit")
+        elif self.current_balance - amount_to_transfer < self.minimum_balance:
+            print("This is below your minimum balance")
+        else:
+            self.current_balance -= amount_to_transfer
+            print("Transfer successful")
