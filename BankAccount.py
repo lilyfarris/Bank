1433531f7a1f53971d0326be8b45254e886e2243
Lily Farris <lilycatherinefarris@hotmail.com>
2026-02-18 12:06:01 -0500

    Added protected variable account_number and private variable routing_number

--- BankAccount.py
+++ BankAccount.py
@@ -1,10 +1,12 @@
 class BankAccount:
     title_of_bank = "Wells Fargo"
 
-    def __init__(self, customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
+        self._account_number = account_number
+        self.__routing_number = routing_number
 
     def deposit(self, amount_to_deposit):
         self.current_balance += amount_to_deposit
@@ -21,18 +23,4 @@ class BankAccount:
         print(self.current_balance)
         print(self.minimum_balance)
 
-
-account1 = BankAccount("Lily", 500, 400)
-account1.deposit(100)
-print(account1.current_balance)
-account1.withdraw(200)
-print(account1.current_balance)
-account1.print_customer_information()
-
-account2 = BankAccount("Anna", 1000, 400)
-account2.deposit(100)
-print(account1.current_balance)
-account2.withdraw(200)
-print(account1.current_balance)
-account2.print_customer_information()
 
