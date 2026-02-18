1e340afc3bf71a6dc68e18954fc0df71693c9421
Lily Farris <lilycatherinefarris@hotmail.com>
2026-02-18 12:45:53 -0500

    Tested transfer below minimum balance and successful transfer

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from SavingsAccount import SavingsAccount
 
 def main():
 
-    account1 = CheckingAccount("Lily Farris", 300, 200, 60, 499, 123)
+    account1 = CheckingAccount("Lily Farris", 300, 200, 120, 499, 123)
 
     #Test transfer above transfer limit
     account1.transfer_money(500)
     #Works Correctly
+
+    #Test transfer below minimum balance
+    account1.transfer_money(110)
+    #Works correctly
+
+    #Test successful transfer
+    account1.transfer_money(60)
+    #Works correctly
 
 main()
